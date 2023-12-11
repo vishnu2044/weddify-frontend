22ec529210c39449d5e31e1f6424c0010a554666
vishnu narayanan <vishnunarayanan2044@gmail.com>
2023-12-11 12:16:16 +0530

    chatapp bug test

--- src/Configure/urls.js
+++ src/Configure/urls.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'https://weddify-backend-api.onrender.com'
-export const wsBaseUrl = 'ws://weddify-backend-api.onrender.com'
+export const wsBaseUrl = 'wss://weddify-backend-api.onrender.com'
 
 // export const baseUrl = 'http://127.0.0.1:8000'
     // export const wsBaseUrl = 'ws://127.0.0.1:8000'
